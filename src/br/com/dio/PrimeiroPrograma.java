b680aff287efd0ac10c5791d673de5e674a2a1be
EduardoAndujar <educasarotto@gmail.com>
2022-02-10 00:12:23 -0200

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato("Bento", "Caramelo claro", 3);
+		Gato gato2 = new Gato("Bento", "Caramelo claro", 3);
+		Gato gato3 = new Gato("Bento", "Caramelo claro", 1);
+		Gato gato4 = gato;
+
+		System.out.println(gato.equals(gato2));
+		System.out.println(gato.equals(gato3));
+		System.out.println(gato.equals(gato4));
+
+		System.out.println(gato);
+
+	}
+
+}
